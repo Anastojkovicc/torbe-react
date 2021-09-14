f5427957287e8f00cfdcb87bc3f5ba28c64ecd6c
Anastojkovicc <ana.stojkovic98@gmail.com>
2021-09-14 14:40:59 +0200

    Stilizovani dugmici

--- torbice/src/components/Welcome.js
+++ torbice/src/components/Welcome.js
@@ -7,6 +7,12 @@ class Pocetna extends Component {
                 <h1>COMBO BAGS</h1>
                 <h2>Poručite torbicu po Vašoj meri!</h2>
                 <p>Vi dizajnirate, mi stvaramo!</p>
+                <p>Instagram: 
+                    <a href="https://www.instagram.com/combo.bags/?igshid=12a5mp93cknnb"> @combo.bags</a>
+                </p>
+                <p>Facebook: 
+                    <a href="https://www.facebook.com/combo.bags1">Ručno rađene torbice</a>
+                     </p>
             </div>
 
         );
